28d16c1709aa61000a0312f029a46d62c9a371ef
Claudia <claudia.lz@outlook.com>
2019-05-19 00:19:08 -0500

    creacion de la tarea fibonnacci

---
+++ FibonacciModelo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class FibonacciModelo
+    {
+
+  
+        public void Datos(int numero)
+        {           
+            
+           
+            int a = 0; int b = 1; int c;
+
+            for (int i = 0; i < numero; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.Write(a + ",");
+
+            }
+
+ 
+
+        }
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                Console.WriteLine("Captura un numero");
+            var numero = Convert.ToInt32(Console.ReadLine());
+
+            FibonacciModelo fibo = new FibonacciModelo();
+            Console.Write("El resultado es: ");
+            fibo.Datos(numero);         
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("El dato no puede convertirse a un número");
+            }
+
+            Console.WriteLine("\n" + "Presiona cualquier tecla para salir");
+            Console.ReadKey();
+
+        }
+    }
+}
